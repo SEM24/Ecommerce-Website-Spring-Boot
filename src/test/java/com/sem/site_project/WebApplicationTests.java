633334e267543bcccf4b06aa24bbaefb543c39aa
SEM laptop <samerx777222@gmail.com>
2022-06-07 15:19:12 +0200

    Initial commit

---
+++ src/test/java/com/sem/site_project/WebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sem.site_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
