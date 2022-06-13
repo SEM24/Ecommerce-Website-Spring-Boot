b540706c63020e7ae1c82320d468589174aba8a4
SEM laptop <samerx777222@gmail.com>
2022-06-13 13:17:11 +0200

    Added: Enum for roles (security)

---
+++ src/main/java/com/khomsi/site_project/entity/Role.java
@@ -0,0 +1,13 @@
+package com.khomsi.site_project.entity;
+
+import org.springframework.security.core.GrantedAuthority;
+
+/* https://stackoverflow.com/questions/19525380/difference-between-role-and-grantedauthority-in-spring-security */
+public enum Role implements GrantedAuthority {
+    USER, ADMIN;
+
+    @Override
+    public String getAuthority() {
+        return name();
+    }
+}
