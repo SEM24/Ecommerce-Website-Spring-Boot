14c7309ff734260cb91ef4b5739a9ab64c9e076f
SEM laptop <samerx777222@gmail.com>
2022-08-02 13:45:52 +0200

    Changed to default setting

--- src/main/java/com/khomsi/site_project/configuration/WebSecurityConfig.java
+++ src/main/java/com/khomsi/site_project/configuration/WebSecurityConfig.java
@@ -24,6 +24,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
         defaultSuccessUrl() – the landing page after a successful login
         failureUrl() – the landing page after an unsuccessful login
      */
+//
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable().authorizeRequests()
